ab39b45795e395bb7388c819dde3d8160cae88e4
Naborisk <naborisk@gmail.com>
2022-06-28 02:40:21 +0900

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app>
+      <v-system-bar></v-system-bar>
+      <v-app-bar color="primary">
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>
+          Gakujo
+        </v-app-bar-title>
+
+        
+      </v-app-bar>
+      <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+      <v-main>
+
+
+        {{notifications}}
+
+      </v-main>
+      <v-footer>
+        
+      </v-footer>
+  </v-app>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity"
+import axios from 'axios'
+
+
+const notifications = ref([])
+
+const fetchNotifications = () => {
+  axios.get('http://localhost:3000/notifications')
+    .then(res => {
+      notifications.value = res.data
+      console.log(res.data)
+    })
+}
+
+fetchNotifications()
+
+</script>
